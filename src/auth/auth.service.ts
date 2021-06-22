e3d83aa87b03a677495bdbcea6435c5855403a33
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2021-06-22 11:51:47 +0200

    implement signin use case

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dtos/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
-
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/tasks/jwt-payload.interface';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const paylaod: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(paylaod);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check your credentials');
+    }
   }
 }
