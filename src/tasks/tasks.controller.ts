48b54cd6986278228293e1c7123f4b44161ef52f
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2021-06-22 14:31:59 +0200

    protect task interaction with authentication layer

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,7 +7,9 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { GetTasksFilterDto } from './dtos/get-tasks-filter.dto';
 import { UpdateTaskDto } from './dtos/update-task.dto';
@@ -15,6 +17,7 @@ import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
   @Get()
